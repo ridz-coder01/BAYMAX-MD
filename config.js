9cabbbd751c657300e314fb3e13e4be5c53cf5a8
Ridz Coder <148397393+ridz-coder01@users.noreply.github.com>
2025-09-08 11:01:49 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,50 @@
+
+// change only what you are ask to change else bit won't work thanks for your understanding 
+const fs = require('fs'), 
+      dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,
+      convertToBool = (text, fault = 'true') => text === fault;
+
+//add your session id
+
+global.session = "https://getyoursessionldforbaymax-d2a7d60642b8.herokuapp.com"; 
+ 
+ 
 module.exports = {
-  SESSION_ID: 'your-session-id', // unique session for the bot
-  PREFIX: '!',                    // command prefix
+SESSION_ID: process.env.SESSION_ID || "", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "263714732501", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+ANTI_DELETE: process.env.ANTI_DELETE || "true", // can be set to inboxonly/allchats/true/false
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
+AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
+AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed By BAYMAX MD", // // Input Yours custom...
+MODE: process.env.MODE || "public", // Put private or public or inbox or groups
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263714732501", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NAME: process.env.OWNER_NAME || "Ridz Coder", // Input Yours custom...(Maintain font for Flow)
+PACK_AUTHOR: process.env.PACK_AUTHOR || "🩵", // Added // Input Yours custom...
+PACK_NAME: process.env.PACK_NAME || "💙", // Added // Input Yours custom...
+PREFIX: process.env.PREFIX || ".",
+VERSION: process.env.VERSION || "1.0.0",
+ANTILINK: process.env.ANTILINK || "true", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTICALL: process.env.ANTICALL || "false",
+ANTIBAD: process.env.ANTIBAD || "false",
+BAD_WORDS: process.env.BAD_WORDS || "null, pm, dm, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
+ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "BAYMAX-MD", //  don't change 
+BOT_PIC: process.env.BOT_PIC || "https://i.ibb.co/mWtJPyP/file-0000000039a461f7940ebd4ffe7226ef.png", //  don't change 
+AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
+AUTO_BIO: process.env.AUTO_BIO || "false",
+AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "i am Baymax md",
+CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
+WELCOME: process.env.WELCOME || "false",
+//not working for the moment do don't on it
+GOODBYE: process.env.GOODBYE || "false", //not working for the moment do don't on it
+AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
+AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Add Multiple Country Codes Separated by Comma...
+PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
+TIME_ZONE: process.env.TIME_ZONE || "Africa/Kampala", // Enter yours else leave blank if not sure
 };
+
+let file = require.resolve(__filename); 
+fs.watchFile(file, () => { fs.unwatchFile(file); console.log(`Update '${__filename}'`); delete require.cache[file]; require(file); });
